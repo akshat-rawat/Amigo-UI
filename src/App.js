76572c91511614dc2247f85bd90ddf59902f3405
Aish Singh <aishv.585@gmail.com>
2021-11-21 11:02:45 +0000

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import './tile/tile'
-import axios from 'axios';
 import Tile from './tile/tile';
 
 export default class App extends React.Component {
@@ -9,23 +8,9 @@ export default class App extends React.Component {
         tracks: []
     }
   
-    componentDidMount() {
-    axios.get(`http://127.0.0.1:8000/api/songs`).then(res => {
-          const tracks = (res.data.songs);
-          this.setState({ tracks });
-        })
-    }
-  
     render() {
     return (
       <div>
-        { this.state.tracks.map(track => <Tile data={{
-                            name: track.name,
-                            artists : track.artists,
-                            album : track.album,
-                            image : track.image,
-                            spotify_link: track.spotify_link
-        }}/>)}
       </div>
     )
     }
---
+++ src/home/home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './home.css';
+
+export default class Home extends React.Component {
+    state = {
+        categories: ["Music","Meme","Movies","Shows","Photos"]
+    }
+    render() {
+    return (
+
+      <div className= "home-container">
+          <Heading/>
+          <div className="button-set">
+        { this.state.categories.map(cat => <CategoryButton data={{name : cat}}/>)}
+        </div>
+      </div>
+    )
+    }
+  }
+
+  class Heading extends React.Component {
+    render() {
+    return (
+        <div className= "heading-container">
+            Mastani Gand
+        </div>
+    )
+    }
+  }
+
+  class CategoryButton extends React.Component {
+    render() {
+    return (
+        <button className="category-button">{this.props.data.name}</button>
+    )
+    }
+  }
+
