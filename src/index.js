77e3f079fed8a68ab7a11a4b5b5f375ab79715cd
Aish Singh <aishv.585@gmail.com>
2021-11-21 11:20:12 +0000

    Setting up

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './home/home';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
